1bc99a18482d8508049731202d57f27c47276a98
Raghavagopanbai <gopanbairaghava@gmail.com>
2025-11-12 14:42:49 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div  id="parent">
+      <h1 id="child1">HI</h1>
+      <h2 id="child2">Hello</h2>
+    </div>
+    <div id="chacha">Big man</div>
+    </>
+  )
+}
+
+export default App
+
